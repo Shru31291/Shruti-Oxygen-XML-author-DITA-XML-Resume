cd377e0bdf2c531353ebcf4f7647e9796c5feb65
Shru31291 <shru31291@gmail.com>
2023-02-22 15:00:41 +0530

    Initial commit

---
+++ oxygen-webhelp/app/search/index/link-to-parent.js
@@ -0,0 +1,2 @@
+/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
+var linkToParent = {11:-1,7:-1,8:7,9:11,6:7,15:-1,10:-1,5:-1,1:-1,12:-1,13:11,14:7,0:11,3:-1,2:-1};
